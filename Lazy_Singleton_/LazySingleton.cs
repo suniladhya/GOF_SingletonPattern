4faad3d1a5bd1e33ef680470c7601e6f17c3e882
Adhya <50034618@acsind.com>
2019-02-18 02:01:51 +0530

    .Net Implementation of Lazy Loading

---
+++ Lazy_Singleton_/LazySingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazy_Singleton_
+{
+    public sealed class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> lazy =
+            new Lazy<LazySingleton>(() => new LazySingleton());
+
+        public static LazySingleton Instance { get { return lazy.Value; } }
+
+        private LazySingleton()
+        {
+        }
+    }
+}
--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using SimpleSingletonPattern;
+﻿using Lazy_Singleton_;
+using SimpleSingletonPattern;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +13,25 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
-            SimpleSingletonPattern.Singleton obj1 = Singleton.Instance;
-            SimpleSingletonPattern.Singleton obj2 = Singleton.Instance;
+            Singleton obj1 = Singleton.Instance;
+            Singleton obj2 = Singleton.Instance;
 
             if (obj1 == obj2)
             {
                 Console.WriteLine("SingleTon Successful");
             }
 
-            ThreadSafeSingletonPattern.ThreadSafeSingleton obj3 = ThreadSafeSingleton.Instance;
-            ThreadSafeSingletonPattern.ThreadSafeSingleton obj4 = ThreadSafeSingleton.Instance;
+            ThreadSafeSingleton obj3 = ThreadSafeSingleton.Instance;
+            ThreadSafeSingleton obj4 = ThreadSafeSingleton.Instance;
 
             if (obj3 == obj4)
+            {
+                Console.WriteLine("SingleTon Successful");
+            }
+            LazySingleton obj5 = LazySingleton.Instance;
+            LazySingleton obj6 = LazySingleton.Instance;
+
+            if (obj5 == obj6)
             {
                 Console.WriteLine("SingleTon Successful");
             }
